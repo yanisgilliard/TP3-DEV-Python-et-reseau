f4784c10260570a0355494257bc124abff1c256a
yanisgilliard <yanisgilliard@gmail.com>
2023-11-06 14:48:10 +0100

    tp-dev3

---
+++ ping_arg.py
@@ -0,0 +1,8 @@
+from os import system 
+from sys import argv 
+
+if __name__ == "__main__":
+    if len(argv) !=2:
+        print ("usage : python3 is_up.py <host>")
+        exit(1)
+    print (system(f"ping {argv[1]}")) 
